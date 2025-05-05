1590b5bae8ae349d6077734ebc5813736eacc649
Alex Casajuana <alex.casajuana@hotjar.com>
2025-05-05 20:41:36 +0200

    adds Wall and HasScore

--- scripts/HasScore.cs
+++ scripts/HasScore.cs
@@ -1,6 +1,20 @@
-namespace godottuto.scripts;
+using Godot;
 
 public interface HasScore
 {
-    
+    StringName Name { get; }
+    int Score { get; set; }
+    Label ScoreDisplay { get; set; }
+    AudioStreamPlayer ScoreSound { get; }
+    void IncrementScore()
+    {
+        Score++;
+        if (ScoreDisplay is not null)
+        {
+            ScoreDisplay.Text = $"{Score}";
+            GD.Print($"{Name} scored. Currently at {Score}.");
+        }
+
+        ScoreSound?.Play();
+    }
 }
--- scripts/Wall.cs
+++ scripts/Wall.cs
@@ -1,6 +1,22 @@
-namespace godottuto.scripts;
+using Godot;
 
-public class Wall
+public partial class Wall : Area2D
 {
-    
+    [Export]
+    public Vector2 BallResetDirection = Vector2.Left;
+
+    [Export]
+    public Node2D Scorer { get; set; }
+
+    public void OnAreaEntered(Area2D area)
+    {
+        if (area is Ball ball)
+        {
+            ball.Reset(BallResetDirection);
+            if (Scorer is HasScore scoring)
+            {
+                scoring.IncrementScore();
+            }
+        }
+    }
 }
